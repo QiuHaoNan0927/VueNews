91e85b5faf4e6b4206a291de4cf28f3c66f902a4
QiuHaoNan0927 <815569451@qq.com>
2017-03-26 22:34:13 +0800

    follow

--- src/components/Column.vue
+++ src/components/Column.vue
@@ -1,25 +1,75 @@
 <template lang="html">
-  <mt-search v-model="value" cancel-text="取消" placeholder="搜索" id="search" :result.sync="result">
-    <mt-cell
-        v-for="item in result"
-        :value="item">
-      </mt-cell>
-  </mt-search>
+  <mt-index-list>
+    <mt-index-section :index="item.index" v-for="item in items">
+      <mt-cell :title="value" v-for="value in item.value"></mt-cell>
+    </mt-index-section>
+  </mt-index-list>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      value: '',
-      result: ['apple', 'bbbbbbb']
+      items: [{
+          index: 'A',
+          value: ['数码', '手机', '电信', 'VR']
+        },
+        {
+          index: 'B',
+          value: ['国际', '国内', '社会', '生活']
+        },
+        {
+          index: 'C',
+          value: ['视频', '评论', '手机', '文化']
+        },
+        {
+          index: 'D',
+          value: ['体育', '房产', '科技', '汽车']
+        },
+        {
+          index: 'E',
+          value: ['数码', '手机', '电信', 'VR']
+        },
+        {
+          index: 'F',
+          value: ['国际', '国内', '社会', '生活']
+        },
+        {
+          index: 'G',
+          value: ['视频', '评论', '手机', '文化']
+        },
+        {
+          index: 'H',
+          value: ['体育', '房产', '科技', '汽车']
+        },
+        {
+          index: 'I',
+          value: ['数码', '手机', '电信', 'VR']
+        },
+        {
+          index: 'J',
+          value: ['国际', '国内', '社会', '生活']
+        },
+        {
+          index: 'K',
+          value: ['视频', '评论', '手机', '文化']
+        },
+        {
+          index: 'L',
+          value: ['体育', '房产', '科技', '汽车']
+        }
+      ],
+
     }
   }
 }
 </script>
 
 <style lang="css" scoped>
-#search{
-  margin-top: 38px;
+.mint-indexlist{
+  padding-top: 38px;
+}
+.mint-indexlist-navlist .mint-indexlist-navitem{
+  padding: 10px 6px;
 }
 </style>
--- src/components/Follow.vue
+++ src/components/Follow.vue
@@ -1,13 +1,54 @@
 <template lang="html">
-<h1 id="h1">Follow.vue</h1>
+  <mt-search v-model="value" cancel-text="取消" placeholder="搜索" id="search">
+    <mt-cell v-for="item in cheakList" :title="item.title" :value="item.value" id="searchItem">
+    </mt-cell>
+  </mt-search>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      value: 'title',
+      result: [{
+          'title': 'title1',
+          'value': 'value1'
+        },
+        {
+          'title': 'title2',
+          'value': 'value2'
+        },
+        {
+          'title': 'title3',
+          'value': 'value3'
+        },
+        {
+          'title': 'title4',
+          'value': 'value4'
+        }
+      ]
+    }
+  },
+  computed: {
+    cheakList() {
+      return this.result.filter(elem => {
+        // body...
+        return elem['title'].indexOf(this.value) > -1;
+      });
+    }
+  }
+}
 </script>
 
-<style lang="css">
-#h1{
-  padding-top: 50px;
+<style lang="css" scoped>
+#search{
+  /*margin-top: 38px;*/
+  padding-top: 38px;
+}
+.mint-search-list-warp a:first-child{
+  margin-top: 38px;
+}
+.mint-search-list-warp >a{
+  border-bottom: 1px solid #ccc;
 }
 </style>
